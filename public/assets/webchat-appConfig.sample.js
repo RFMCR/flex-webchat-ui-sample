2c0739c19aad446f1c9422f1b2c61018554b899a
Roberto Fernandez <betof01@hotmail.com>
2022-03-08 15:08:40 -0600

    Updating sample with current code

--- public/assets/webchat-appConfig.sample.js
+++ public/assets/webchat-appConfig.sample.js
@@ -5,5 +5,86 @@ var appConfig = {
     flexFlowSid: "FO...",
     colorTheme: {
         overrides: brandedColors
+    },
+    disableLocalStorage:false,
+    available:true,
+    componentProps: {
+        // MessagingCanvas: {
+        //     avatarCallback: (identity) => SessionActions.getAgentAvatar(identity),  //It breaks here! Code taken from https://www.twilio.com/docs/flex/developer/messaging/webchat/configuration
+        //     showTrayOnInactive: true 
+        // },   
+        MessageCanvasTray: {
+             onButtonClick: () => Actions.invokeAction("RestartEngagement") //It breaks here! Code taken from https://www.twilio.com/docs/flex/developer/messaging/webchat/configuration
+            //onButtonClick: () => {alert("I am getting here")} //This works
+
+            //Other resources 
+            //https://www.twilio.com/docs/flex/developer/messaging/webchat/setup
+
+        }        
+    },
+
+    tokenServerUrl: "https://iam.twilio.com/v1/Accounts/{accountSid}/Tokens",
+    flexWebChannelsUrl: "https://flex-api.twilio.com/v1/WebChannels",
+    context: {
+        friendlyName: "Anonymous"
+    },
+    startEngagementOnInit: false,
+    preEngagementConfig: {
+        description: "Please provide some information",
+        fields: [
+            {
+                label: "What is your name?",
+                type: "InputItem",
+                attributes: {
+                    name: "friendlyName",
+                    type: "text",
+                    required: true,
+                    placeholder:"Enter your name",
+                }
+            },
+            {
+                label:"What is your email?",
+                type: "InputItem",
+                attributes:{
+                    name:"email",
+                    type:"email",
+                    placeholder:"Enter your email",
+                    required:true,
+                }                
+            },
+            {
+                label:"My awesome dropdown",
+                type:"SelectItem",
+                attributes:{
+                    name:"My Awesome dropdown",
+                    required:true,
+                },
+                options:[
+                    {
+                        value:"value1",
+                        label:"label1",
+                        selected:false
+                    },
+                    {
+                        value:"value2",
+                        label:"label2",
+                        selected:true
+                    },
+                ]
+            },
+            {
+                label: "What is your question?",
+                type: "TextareaItem",
+                attributes: {
+                    name: "question",
+                    type: "text",
+                    placeholder: "Type your question here",
+                    required: false,
+                    rows: 5
+                }
+            }, 
+        ],
+        footerLabel:"I am a footer",
+        submitLabel: "Ok Let's Go!"
     }
 }
--- src/App.js
+++ src/App.js
@@ -10,13 +10,67 @@ class App extends React.Component {
 
     const { configuration } = props;
     FlexWebChat.Manager.create(configuration)
-      .then(manager => this.setState({ manager }))
+      .then((manager) => {
+        // Add the EndChat component into FlexWebChat
+        // FlexWebChat.MainHeader.Content.add(<MinimizeButton key="mimize-chat" />, { sortOrder: -1, align: "end" });
+        // FlexWebChat.MainHeader.Content.add(<CloseButton key="close-chat" runtimeDomain={AppConfig.current().runtimeDomain} manager={manager} />, { sortOrder: -1, align: "end" });
+        // FlexWebChat.MainHeader.Content.remove("close-button");
+        this.setState({ manager })
+      })
       .catch(error => this.setState({ error }));
+
+
   }
 
   render() {
     const { manager, error } = this.state;
+
     if (manager) {
+
+      FlexWebChat.MessagingCanvas.defaultProps.predefinedMessage = false;
+      FlexWebChat.MainHeader.defaultProps.titleText = "Habla con nosotros"
+
+      FlexWebChat.Actions.on("afterStartEngagement", (payload) => {
+        const { question } = payload.formData;
+        console.log(FlexWebChat.Actions)
+        if (!question) return;
+
+        const { channelSid } = manager.store.getState().flex.session;
+
+        manager.chatClient.getChannelBySid(channelSid)
+          .then(channel => {
+            channel.sendMessage(question)
+          })
+
+      })
+
+      
+      manager.strings.EntryPointTagline = "Contactanos!!";
+      manager.strings.MessageCanvasTrayContent = `
+          <h6>Gracias por contactarnos!</h6>
+          <p>Si tiene mas dudas, no dude en chatear con nosotros de nuevo.</p>`;
+      manager.strings.MessageCanvasTrayButton = "Comanzar Chat";
+      manager.strings.InvalidPreEngagementMessage = "Pre-engagement forms have not been set and are required to initiate the web-chat. Please configure these now in setup.";
+      manager.strings.InvalidPreEngagementButton = "Boton Invalido";
+      manager.strings.PredefinedChatMessageAuthorName = "Bot";
+      manager.strings.PredefinedChatMessageBody = "Hola, en que te ayudo?";
+      manager.strings.InputPlaceHolder = "Tu mensaje Aqui!";
+      manager.strings.TypingIndicator = "{{name}} esta escribiendo ... ";
+      manager.strings.Read = "Leer";
+      manager.strings.MessageSendingDisabled = "El envio de mensajes se deshabilito";
+      manager.strings.Today = "Hoy";
+      manager.strings.Yesterday = "Ayer";
+      manager.strings.MessageCanvasTrayButton = "COMENZAR NUEVO";
+      manager.strings.WelcomeMessage = "Mensaje de Bienvenida";
+      manager.strings.Save = "SALVER";
+      manager.strings.Reset = "RESETEAER";
+      manager.strings.MessageCharacterCountStatus = "{{currentCharCount}} / {{maxCharCount}}";
+      manager.strings.SendMessageTooltip = "Enviar Mensaje";
+      manager.strings.FieldValidationRequiredField = "Campo requerido";
+      manager.strings.FieldValidationInvalidEmail = "Direccion invalida";
+
+
+
       return (
         <FlexWebChat.ContextProvider manager={manager}>
           <FlexWebChat.RootContainer />
